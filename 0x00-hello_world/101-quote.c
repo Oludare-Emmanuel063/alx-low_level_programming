d5a2c9befc7cf2b0fa4dcf7385fb7898880478d4
Oludare-Emmanuel063 <Oludareemmanuel063@gmail.com>
2023-07-16 19:38:45 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 /*
  * File: 101-quote.c
- * Auth: Oludare-Emmanuel063
+ * Auth: Oludare-Emmanuel063 @ALXSWE
  */
 
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"               
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
