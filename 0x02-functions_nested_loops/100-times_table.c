d426f3e78460cb71765acf0eda4624362a3e75f8
Oludare-Emmanuel063 <oludareemmanuel063@gmail.com>
2023-07-20 07:54:02 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,50 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Prints natural numbers below 1024 that are
- * multiplies of 3 or 5
+ * print_times_table - Prints the n times table
  *
- * Return: Always 0.
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
-int main(void)
+void print_times_table(int n)
 {
-	int a, b;
+	int x = 0, y, z;
 
-	for (a = 1; a < 1024; a++)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		if ((a % 3) == 0 || (a % 5) == 0)
-			b += a;
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
 	}
-	printf("%d\n", b);
-	return (0);
 }
