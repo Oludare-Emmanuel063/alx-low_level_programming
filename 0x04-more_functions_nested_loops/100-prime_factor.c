e96971497e9c26d2a99e742fcf34833236d86404
Oludare-Emmanuel063 <oludareemmanuel063@gmail.com>
2023-07-20 15:02:37 -0700

    more functiond mandatory task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	long number = 612852475143;
+
+	for (a = (int) sqrt(number); a > 2; a++)
+	{
+		if (number % a == 0)
+		{
+			printf("%d\n", a);
+			break;
+		}
+	}
+
+	return (0);
+}
