a7661d91bd439f59afa2d8c9e0158871c05ce9af
Oludare-Emmanuel063 <oludareemmanuel063@gmail.com>
2023-08-24 22:42:39 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements
+ * @h: list with single link
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num_0f_node = 0;
+
+	while (h != NULL)
+	{
+		h =h->next;
+		num_of_node++;
+	}
+	return (num_of_node);
+}
